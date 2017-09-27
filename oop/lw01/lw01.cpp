1afb3459b0bff449d3095bf94533bb8130cfdb13
Mivallion <mivallion@gmail.com>
2017-09-27 10:40:45 +0300

    Добавлен вывод для понимания пользователем, что происходит

--- oop/lw01/lw01.cpp
+++ oop/lw01/lw01.cpp
@@ -11,20 +11,22 @@
 // Для ромба задаются длины его диагоналей
 int main(int argc, char** argv)
 {
+	std::cout << "Enter sides of rectangle: " << std::endl;
 	Figure *rectangle = new Rectangle(std::cin);
 	rectangle->Print();
 	std::cout << rectangle->Square() << std::endl;
 	delete rectangle;
 
+	std::cout << "Enter sides of trapeze: " << std::endl;
 	Figure *trapeze = new Trapeze(std::cin);
 	trapeze->Print();
 	std::cout << trapeze->Square() << std::endl;
 	delete trapeze;
-
+	
+	std::cout << "Enter diagonales of rhombus: " << std::endl;
 	Figure *rhombus = new Rhombus(std::cin);
 	rhombus->Print();
 	std::cout << rhombus->Square() << std::endl;
 	delete rhombus;
     return 0;
 }
-
